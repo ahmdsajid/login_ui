bc62457464a62f806dd38e0c905e5c0c455ffc3f
Mahmudul Sajid <155292895+ahmdsajid@users.noreply.github.com>
2024-09-05 15:19:09 +0600

    update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,16 +10,20 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  bool isobscure = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
             image: DecorationImage(
                 fit: BoxFit.cover,
                 image: NetworkImage(
-                    "https://img.freepik.com/premium-photo/blue-background-with-stars-place-text_1034303-438625.jpg?w=360"))),
+                    "https://img.freepik.com/premium-photo/pink-purple-colored-liquid-with-pink-purple-background_1034303-438946.jpg?w=360"))),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -35,11 +41,75 @@ class _LoginPageState extends State<LoginPage> {
               height: 500,
               
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(40),
                   topRight: Radius.circular(40)
                 )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Column(children: [
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                
+                      prefixIcon: Icon(Icons.email),
+                      hintText: "Enter your valid Email",
+                      
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        borderSide: BorderSide(color: Colors.blue)
+                      ),
+                    ),  
+                  ),
+
+                  SizedBox(height: 30,),
+
+                  TextFormField(
+                    obscureText: isobscure,
+                    decoration: InputDecoration(
+                
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: MaterialButton(onPressed: (){
+                        setState(() {
+                          isobscure = !isobscure;
+                        });
+                      }, child: Icon(Icons.visibility),),
+                      hintText: "Enter your Password",
+                      
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        borderSide: BorderSide(color: Colors.blue)
+                      ),
+                    ),  
+                  ),
+
+                  SizedBox(height: 20,),
+
+                  Text("Forgot your password?"),
+
+                  Spacer(),
+
+                  MaterialButton(
+                    minWidth: 350,
+                    height: 40,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                    color: Colors.blueAccent,
+                    onPressed: (){}, child: Text("Sign in",style: TextStyle(color: Colors.white),),
+                    ),
+
+                    
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: RichText(text: TextSpan(
+                        text: "Don't have account?",style: TextStyle(color: Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(text: "Sign Up",style: TextStyle(color: Colors.blue))
+                        ]
+                      )),
+                    )
+                ],),
               ),
             )
             ],
